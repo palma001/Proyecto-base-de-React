2ad73e370d933ef4df857ea4feab5c0f01c0ef48
Alexis Hernandez <alexisrhc@hotmail.com>
2024-12-10 14:45:15 -0400

    install react router v7

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import AuthLayout from "./layouts/AuthLayout";
+import App from "./pages/App";
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={'inicio de la app'} />
+
+        <Route path="authentication" element={<AuthLayout />}>
+          <Route path="login" element={<App />} />
+          <Route path="register" element={'registro'} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
