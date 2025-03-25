bd057b7c213be0d3c84697ccca4749390cbea467
Jose Rodriguez <rodriguezjoseagustin30@gmail.com>
2025-03-25 14:49:52 -0400

    fix: country lead added in Zod

--- src/pages/admin/metadata/configs/rules/leadRule.ts
+++ src/pages/admin/metadata/configs/rules/leadRule.ts
@@ -6,6 +6,7 @@ export const leadRule = z.object({
   last_name: z.string().min(1, { message: "El apellido es obligatorio" }),
   phone_number: z.string().min(1, { message: "El teléfono es obligatorio" }),
   status: z.string().min(1, { message: "El estado es obligatorio" }),
+  country: z.string().min(1, { message: "El país es obligatorio" }),
   description: z.string().min(1, { message: "La descripción es obligatoria" }),
   company_name: z.string().min(1, { message: "La empresa es obligatoria" }),
 });
